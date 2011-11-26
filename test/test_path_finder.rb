234bb8dbbca5acb5e94b988b2e986e3a617d4346
Andrei Beliankou <arbox@yandex.ru>
2011-11-26 19:44:40 +0100

    Added a sample test file, we can grasp the main idea behind unit testing.

---
+++ test/test_path_finder.rb
@@ -0,0 +1,33 @@
+require 'chatbot/path_finder'
+require 'test/unit'
+
+require 'test/test_helper.rb'
+
+class TestPathFinder < Test::Unit::TestCase
+  include TestHelper
+  
+  def setup
+    @finder = Chatbot::PathFinder.new(file)
+  end
+
+  def teardown
+    # after every test
+  end
+
+  def test_shortest_pat
+    @finder.xxx
+    assert()
+    assert_match
+    asser_equal
+    assert_raise
+    assert_nothig_raised
+    assert_block
+    assert_respond_to
+    # ...
+  end
+
+
+  private
+  def xxx
+  end
+end
