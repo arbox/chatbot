e14580c51bb1685e21c5412054bca970eeb33d6d
unknown <Daniel@visum.(none)>
2011-11-30 18:03:56 +0100

    added ConfigReader.read (erster Vorschlag)

--- lib/chatbot/ConfigReader.rb
+++ lib/chatbot/ConfigReader.rb
@@ -1,11 +1,32 @@
 module Chatbot
+  #Klasse zum einlesen von Konfigurationsdateien im YAML-Format.
+  
   class ConfigReader
     def initialize(bot)
-      
-    end
-    # @bot
-    # yaml ...
-    # reads yaml configuration files
-    # @bot.add_command  {}
-  end
+		@bot = bot
+		
+		#Liste der einzulesenden Dateien
+		@configfiles = ["abschied.yml"]
+    end #end def initialize
+	
+	#Iteriert über alle Dateien und erstellt Befehle für den Bot.
+	#Die Befehle selbst reagieren auf den RegExp-Teil der aus den YAML-Dateien
+	#ausgelesenen Zeilen. Der Rückgabewert ist jeweils der definierte Wert als 
+	#einfacher String innerhalb der YAML-Datei.
+	def read()		
+		@configfiles.each do |filename|
+			@yfile = YAML.load_file(filename)
+			
+			@yfile.each do |key, value| 
+				@bot.add_command(
+					:syntax      => key,
+					:description => "reacts on RegExp /#{key}/, returning the String: #{value}",
+					:regex       => Regexp.new(key),
+					:is_public   => true
+				) { value }
+			end			
+		end
+	end #end def read
+  end #end Class
+  
 end
