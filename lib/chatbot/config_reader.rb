d20324280dd4469acec7ab34e88cdb6d3b0ad626
unknown <Daniel@visum.(none)>
2011-12-10 02:17:15 +0100

    teach so geaendert, dass answer_question gesondert behandelt wird (da hier noch die Nutzereingabe mitgeliefert werden soll).

--- lib/chatbot/config_reader.rb
+++ lib/chatbot/config_reader.rb
@@ -12,16 +12,12 @@ module Chatbot
 	#mit den aufzurufenden Befehlen.
 	#Es werden alle Dateien mit Namen "chat_*.yml" im
 	#Verzeichnis chatbot eingelesen
-	def read()					
-		Dir["chatbot/chat_*.yml"].each do |filename|
+	def read()
+		Dir["etc/chatbot/chat_*.yml"].each do |filename|
 			@yfile = YAML.load_file(filename)
 			
-			@yfile.each do |key, value| 	
-				if @bot.respond_to?(value) 
-					@commands[key] = @bot.method(value)
-				else
-					@commands[key] = @bot.method("can_i_help")
-				end
+			@yfile.each do |key, value|
+				@commands[key] = value
 			end			
 		end
 		
@@ -30,14 +26,34 @@ module Chatbot
 	
 	#Wandelt die vorher eingelesenen Befehle in Befehle
 	#für den Chatbot um (add_command)
+	#Ist die angegebene Funktion im Bot bekannt, so wird diese Aufgerufen.
+	#Ansonsten wird der String als Antwort geliefert.
 	def teach()
 		@commands.each do |key, meth|
-			@bot.get_bot.add_command(
-				:syntax      => key,
-				:description => "reacts on RegExp /#{key}/, invoking the Method: #{meth.name}",
-				:regex       => Regexp.new(key),
-				:is_public   => true
-			) { meth.call }
+			if meth == "answer_question"
+				@bot.get_bot.add_command(
+					:syntax      => key,
+					:description => "reacts on RegExp /#{key}/, invoking the Method: #{meth}",
+					:regex       => Regexp.new(key, Regexp::IGNORECASE),
+					:is_public   => true
+				) do |sender, params, msg|
+					@bot.answer_question(sender, msg)
+				  end
+			elsif @bot.respond_to?(meth)
+				@bot.get_bot.add_command(
+					:syntax      => key,
+					:description => "reacts on RegExp /#{key}/, invoking the Method: #{meth}",
+					:regex       => Regexp.new(key, Regexp::IGNORECASE),
+					:is_public   => true
+				) { @bot.method(meth).call }
+			else
+				@bot.get_bot.add_command(
+					:syntax      => key,
+					:description => "reacts on RegExp /#{key}/, returing the String: #{meth}",
+					:regex       => Regexp.new(key, Regexp::IGNORECASE),
+					:is_public   => true
+				) { meth }			
+			end
 	    end		
 	end #end def teach
 	
